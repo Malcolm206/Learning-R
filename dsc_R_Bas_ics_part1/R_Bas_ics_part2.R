5d125107e3d61ac44b8dbe44923a9a05506b57e0
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2021-03-08 16:40:35 -0500

    second lesson in R

---
+++ dsc_R_Bas_ics_part1/R_Bas_ics_part2.R
@@ -0,0 +1,13 @@
+2 + 2
+sum(2,2)
+base::sum(2,2)
+
+lsf("package:base")
+dplyr::if_else
+
+convert_f_to_c <- function(farh_number) {
+  celc <- (farh_number - 32) / 1.8
+  celc
+}
+
+convert_f_to_c(87)
